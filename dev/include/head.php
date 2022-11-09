45d75d26039684f77df7b4f1ef58d0e46744316b
Léo <69589034+Zomblard@users.noreply.github.com>
2022-11-09 09:57:58 +0100

    [ADD] flowbite

--- dev/include/head.php
+++ dev/include/head.php
@@ -7,6 +7,7 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="assets/css/style.css">
+    <script src="/dev/assets/js/flowbite.js"></script>
     <script src="https://kit.fontawesome.com/eced519d56.js" crossorigin="anonymous"></script>
 
 </head>
