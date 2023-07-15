b1ddb74d0997437755f91cd84cd29a59d5561cd0
Léo <69589034+Zomblard@users.noreply.github.com>
2023-07-15 19:17:58 +0200

    [Add] Captcha forgot password

--- Views/Users/forgot_password.view.php
+++ Views/Users/forgot_password.view.php
@@ -1,5 +1,6 @@
 <?php
 
+use CMW\Controller\Core\SecurityController;
 use CMW\Manager\Security\SecurityManager;
 use CMW\Model\Core\ThemeModel;
 use CMW\Utils\Website;
@@ -34,6 +35,7 @@ $description = Website::getWebsiteDescription();
             </div>
             <input type="email" name="mail" id="input-group-1" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="votre@mail.com">
         </div>
+            <?php SecurityController::getPublicData(); ?>
         <button type="submit" class="w-full mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Envoyer</button>
         </form>
     </div>
