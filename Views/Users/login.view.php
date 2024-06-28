bdca824dc5a6edd5312dc330dec4ae10cbffdf73
Léo <69589034+Zomblard@users.noreply.github.com>
2024-06-29 00:51:40 +0200

    [ADD] Enforce 2fa

--- Views/Users/login.view.php
+++ Views/Users/login.view.php
@@ -11,7 +11,7 @@ Website::setTitle("Connexion");
 Website::setDescription("Connectez-vous sur " . Website::getWebsiteName());
 ?>
 
-<section style="background-image: url('<?= ThemeModel::fetchImageLink('hero_img_bg') ?>');" class="bg-cover mb-4">
+<section style="background-image: url('<?= ThemeModel::getInstance()->fetchImageLink('hero_img_bg') ?>');" class="bg-cover mb-4">
     <div class="text-center text-white py-8">
         <h2 class="font-bold">Connexion</h2>
         <p>Description de la page</p>
